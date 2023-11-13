488b01fd68d2e15fb0136ddb49cdde02733f1c65
Chris <miller.christopher618@gmail.com>
2023-11-13 02:23:32 -0600

    fix(Update): Adding Fibonacci

---
+++ Fibonacci/Fibonacci/Fibonacci/Program.cs
@@ -0,0 +1,22 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.Write("Enter a Number: ");
+        int number = Convert.ToInt32(Console.ReadLine());
+        int firstnumber = 0; int secondnumber = 1; int temp = 0;
+        Console.WriteLine(firstnumber);
+        Console.WriteLine(secondnumber);
+        while (temp <= number)
+        {
+            temp = firstnumber + secondnumber;
+            if (temp > number) 
+            {
+                break;
+            }
+            Console.WriteLine(temp);
+            firstnumber = secondnumber; 
+            secondnumber = temp;
+        } 
+    }
+}
