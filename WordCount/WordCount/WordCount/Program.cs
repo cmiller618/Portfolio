a2191ab692146a83e6c32ac15ac41c83d4c90cdc
Chris <miller.christopher618@gmail.com>
2023-11-13 01:10:17 -0600

    fix(Updated): Add Word Count

---
+++ WordCount/WordCount/WordCount/Program.cs
@@ -0,0 +1,13 @@
+﻿using System.Text.RegularExpressions;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Reading Text File...");
+        var phrase = File.ReadAllText(@"C:\Users\chris\Documents\Portfolio\WordCount\WordCount\WordCount\TextFile.txt");
+        phrase = Regex.Replace(phrase, @"[^\w\d\s]", "");
+        var wordCount = phrase.Split(' ').Length;
+        Console.WriteLine(wordCount);
+    }
+}
