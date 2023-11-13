227e013657c7c94592adb809a8cb811b140fdece
Chris <miller.christopher618@gmail.com>
2023-11-12 21:38:14 -0600

    fix(Update): Adding ReverseString

---
+++ FizzBuzz/ReverseString/ReverseString/ReverseString/Program.cs
@@ -0,0 +1,16 @@
+﻿public class Program
+{
+    public static void Main(String[] args)
+    {
+        Console.Write("Enter a String: ");
+        string phrase = Console.ReadLine();
+        var phraseList = phrase.ToList();
+        int end = phraseList.Count - 1;
+        string newPhrase = "";
+        for(int i = end; i >= 0; i--)
+        {
+            newPhrase += phraseList[i];
+        }
+        Console.WriteLine(newPhrase);
+    }
+}
