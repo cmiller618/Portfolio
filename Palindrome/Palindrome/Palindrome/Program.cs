ab6ea935c50e57d75132baee4bf72d47ee38d6bd
Chris <miller.christopher618@gmail.com>
2023-11-13 00:24:17 -0600

    fix(Updated): Adding Palindrome

---
+++ Palindrome/Palindrome/Palindrome/Program.cs
@@ -0,0 +1,23 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.Write("Enter a word or phrase: ");
+        var phrase = Console.ReadLine().ToLower();
+        var reversed = "";
+        var phraseList = phrase.ToList();
+        for(int i = phraseList.Count - 1; i >= 0; i--)
+        {
+            reversed += phraseList[i];
+        }
+
+        if (reversed.Equals(phrase))
+        {
+            Console.WriteLine("Your phrase is a palindrome");
+        }
+        else
+        {
+            Console.WriteLine("Your phrase is not a palindrome");
+        }
+    }
+}
