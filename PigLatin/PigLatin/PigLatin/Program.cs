7dd0e9af25b7de3a5730bc66c7ea7ef7ef85bc8f
Chris <miller.christopher618@gmail.com>
2023-11-12 23:12:36 -0600

    fix(Update): Adding Pig Latin Converter

---
+++ PigLatin/PigLatin/PigLatin/Program.cs
@@ -0,0 +1,56 @@
+﻿public class Program
+{
+    public static List<char> Vowels = new List<char>() {'a', 'e', 'i', 'o', 'u'};
+    public static List<char> Punctuation = new List<char> { ',', ' ', ';', '.' };
+    public static string vowelEnding = "yay";
+    public static string standardEnding = "ay";
+    public static void Main(string[] args)
+    {
+        Console.Write("Enter a Phrase: ");
+        string phrase = Console.ReadLine();
+        var splitPhrase = phrase.Split(new[] { ',', ' ', ';', '.' }).ToList();
+        var splitPhraseWithPunctuation = phrase.ToList();
+        var punctList = new List<char>();
+        string newPhrase = String.Empty;
+        for(int i = 0; i < splitPhrase.Count; i++)
+        {
+            newPhrase = phrase.Replace(splitPhrase[i], ConvertWord(splitPhrase[i]));
+        }
+
+        Console.WriteLine(newPhrase);
+
+    }
+
+    public static string ConvertWord(string word)
+    {
+        var splitWord = word.ToList();
+        if (Vowels.Contains(splitWord[0]))
+        {
+            return word + vowelEnding;
+        }
+
+        string beginning = "";
+        for(int i = 0; i < splitWord.Count; i++)
+        {
+            if (!Vowels.Contains(splitWord[i]))
+            {
+                beginning += splitWord[i];
+                splitWord[i] = '\0';
+            }
+            else
+            {
+                break;
+            }
+
+        }
+
+        string newWord = "";
+        for(int i = 0; i < splitWord.Count; i++)
+        {
+            newWord += splitWord[i];
+        }
+
+        return newWord + beginning + standardEnding;
+
+    }
+}
